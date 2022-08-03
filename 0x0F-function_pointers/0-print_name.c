f2369f02a5ded2d1f3070eda42760bee4d82a256
Theophilus Paintsil <tpee935@gmail.com>
2022-08-03 16:19:35 +0000

    Update 0-print_name.c
    Updated file changes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name.
  * @name: name to print
  * @f: fi=unctu=ion used
  * Rteurn void
  */
+
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL && name != NULL)
-f(name);
+if (f != NULL)
+    f(name);
 }
